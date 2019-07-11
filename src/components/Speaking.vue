37d8f16dba08cd6880b142c3023053264b0306d5
Lekha Surasani <lesurasani@gmail.com>
2019-07-10 21:30:55 -0400

    added new articles & slides

--- src/components/Speaking.vue
+++ src/components/Speaking.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <vs-row vs-justify="center">
-            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
                 <vs-card class="cardx">
                     <div slot="header">
                     <h3>
@@ -17,12 +17,13 @@
                     <div slot="footer">
                         <vs-row vs-justify="flex-end">
                             <vs-button href="https://github.com/lsurasani/react-hooks-examples" target="_blank" color="rgb(44,62,80)" icon="code"></vs-button>
-                            <vs-button href="https://www.meetup.com/React-ATL/events/259705192/" target="_blank" color="rgb(44,62,80)" text-color="rgb(255,255,255)" icon="open_in_new"></vs-button>
+                            <vs-button href="https://docs.google.com/presentation/d/1sae6auAGUVhg64KLwyl9J1cUoiqhbO0MP6UXED49flk/edit?usp=sharing" target="_blank" color="rgb(44,62,80)" text-color="rgb(255,255,255)" icon="open_in_new"></vs-button>
                         </vs-row>
                     </div>
                 </vs-card>
             </vs-col>
-            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="1" />
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
                 <vs-card class="cardx">
                     <div slot="header">
                     <h3>
@@ -39,29 +40,50 @@
                     <div slot="footer">
                     <vs-row vs-justify="flex-end">
                         <vs-button href="https://github.com/lsurasani/pwa-example" target="_blank" color="rgb(44,62,80)" icon="code"></vs-button>
-                        <vs-button href="https://www.meetup.com/gdg-atlanta/events/cbwcpqyzfbkc/" target="_blank" color="rgb(44,62,80)" text-color="rgb(255,255,255)" icon="open_in_new"></vs-button>
+                        <vs-button href="https://docs.google.com/presentation/d/171XaOrC2a2HkBcbMtNgMgV6CKrsxjLSKDRJRh24QxEQ/edit?usp=sharing" target="_blank" color="rgb(44,62,80)" text-color="rgb(255,255,255)" icon="open_in_new"></vs-button>
                     </vs-row>
                     </div>
                 </vs-card>
             </vs-col>
-            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
                 <vs-card class="cardx">
                     <div slot="header">
                     <h3>
-                        Create a PWA - WeRockITConf, May 2019
+                        Advanced Topics in React - WeRockITConf, May 2019
                     </h3>
                     </div>
                     <div slot="media">
                     <img src="">
                     </div>
                     <div>
-                    <span>Learn more about what a PWA is and how to make one.
-                        We’ll talk about some of the disadvantages of using a PWA, and whether using a PWA is right for you.</span>
+                    <span>Learn more about Hooks, Suspense, Context, and HOC's!</span>
                     </div>
                     <div slot="footer">
                     <vs-row vs-justify="flex-end">
                         <vs-button href="https://stackblitz.com/@lsurasani" target="_blank" color="rgb(44,62,80)" icon="code"></vs-button>
                         <vs-button href="https://docs.google.com/presentation/d/1B4oHPEU9Wm3S9DPCgnPjAuMbDSl0bPGPyahwaBbNuyM/edit?usp=sharing" target="_blank" color="rgb(44,62,80)" text-color="rgb(255,255,255)" icon="open_in_new"></vs-button>
+                    </vs-row>
+                    </div>
+                </vs-card>
+            </vs-col>
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="1" />
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
+                <vs-card class="cardx">
+                    <div slot="header">
+                    <h3>
+                        Creating a PWA Workshop - WeRockITConf, May 2019
+                    </h3>
+                    </div>
+                    <div slot="media">
+                    <img src="">
+                    </div>
+                    <div>
+                    <span>What's a PWA, how do you make one, should you make one?</span>
+                    </div>
+                    <div slot="footer">
+                    <vs-row vs-justify="flex-end">
+                        <vs-button href="" target="_blank" color="rgb(44,62,80)" icon="code"></vs-button>
+                        <vs-button href="https://docs.google.com/presentation/d/1qjkEgEp9Upbdw-Q5tTLCmum4rmruoAW7Q5f63kSA__o/edit?usp=sharing" target="_blank" color="rgb(44,62,80)" text-color="rgb(255,255,255)" icon="open_in_new"></vs-button>
                     </vs-row>
                     </div>
                 </vs-card>
--- src/components/Writing.vue
+++ src/components/Writing.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <vs-row vs-justify="center">
-            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
                 <vs-card class="cardx">
                     <div slot="header">
                     <h3>
@@ -13,6 +13,47 @@
                     </div>
                     <div>
                         <span>Thinking about migrating to React Router 4? This article describes the process.</span>
+                    </div>
+                    <div slot="footer">
+                        <vs-row vs-justify="flex-end">
+                            <vs-button href="https://medium.com/@lesurasani/a-guide-to-upgrading-to-react-router-4-bc064f3cbdb9" target="_blank" color="rgb(44,62,80)" text-color="rgb(255,255,255)" icon="open_in_new"></vs-button>
+                        </vs-row>
+                    </div>
+                </vs-card>
+            </vs-col>
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="1" />
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
+                <vs-card class="cardx">
+                    <div slot="header">
+                    <h3>
+                        Introduction to React Hooks
+                    </h3>
+                    </div>
+                    <div slot="media">
+                        <img src="">
+                    </div>
+                    <div>
+                        <span>A Quick Introduction to React Hooks and how to use them.</span>
+                    </div>
+                    <div slot="footer">
+                        <vs-row vs-justify="flex-end">
+                            <vs-button href="https://medium.com/@lesurasani/a-guide-to-upgrading-to-react-router-4-bc064f3cbdb9" target="_blank" color="rgb(44,62,80)" text-color="rgb(255,255,255)" icon="open_in_new"></vs-button>
+                        </vs-row>
+                    </div>
+                </vs-card>
+            </vs-col>
+            <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="5">
+                <vs-card class="cardx">
+                    <div slot="header">
+                    <h3>
+                        Why New Developers Should Work at Small Startups
+                    </h3>
+                    </div>
+                    <div slot="media">
+                        <img src="">
+                    </div>
+                    <div>
+                        <span>I spent my first year of my career working at a small startup and gained a lot from it! Here's my case for why you should try it, too!</span>
                     </div>
                     <div slot="footer">
                         <vs-row vs-justify="flex-end">
